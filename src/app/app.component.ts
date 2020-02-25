a4270e5471e58f6246fc1318d54be2f06e99b8f8
shreyashejwalkar <shreyashejwalkar1610@gmail.com>
2020-02-25 14:10:02 +0530

    Binding data to model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Template driven forms';
   topics =["Angular", "React", "Vue"];
+  
+  userModel = new User('Shreya', 'abc@gmail.com', 4455887799, '', 'morning', true);
 }
